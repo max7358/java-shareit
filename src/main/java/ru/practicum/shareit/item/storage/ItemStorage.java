f6639d846e529c03e35aac6c00517c3f1ac88d39
Max <maxusjob@ya.ru>
2024-08-09 20:08:35 +0200

    code style fix

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.storage;
 
 import ru.practicum.shareit.common.storage.CommonStorage;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,8 @@
 package ru.practicum.shareit.request;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 
 @Slf4j
 @RestController
