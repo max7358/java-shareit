d0ef79a848849c00ee2de6946d5626a48e63b6d3
Max <maxusjob@ya.ru>
2024-08-09 20:18:14 +0200

    tests fix

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -5,10 +5,10 @@ import ru.practicum.shareit.user.model.User;
 
 public class UserMapper {
     public static UserDto toUserDto(User user) {
-        return new UserDto(user.getId(), user.getName(), user.getEmail());
+        return new UserDto(user.getId(), user.getEmail(), user.getName());
     }
 
     public static User toUser(UserDto userDto) {
-        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+        return new User(userDto.getId(), userDto.getEmail(), userDto.getName());
     }
 }
